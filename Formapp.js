50b80d0dc0685183aa8a3938e9b1b6d586252f43
antonanderseneal <anto0718@edu.eal.dk>
2017-10-31 11:30:59 +0100

    nyændring6

--- Formapp.js
+++ Formapp.js
@@ -9,7 +9,7 @@ validationApp.controller('mainController', function($scope)
 
     // check to make sure the form is completely valid
     if (isValid) {
-      alert("ny aendring5"); 
+      alert("ny aendring5 ny ændring 6"); 
     }
 
   };
