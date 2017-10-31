5dd20df5713f7ccb8b3aca549d74b21fdaef3956
antonanderseneal <anto0718@edu.eal.dk>
2017-10-31 10:04:07 +0100

    antonchange3

--- Formapp.js
+++ Formapp.js
@@ -4,4 +4,14 @@ var validationApp = angular.module('validationApp', []);
 
 // create angular controller
 validationApp.controller('mainController', function($scope) 
-						 
+
+ {
+
+    // check to make sure the form is completely valid
+    if (isValid) {
+      alert('our form is amazing');
+    }
+
+  };
+
+});
